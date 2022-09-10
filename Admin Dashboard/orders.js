e5f36da7af309d9f49f840d974e9fe62ebd3cc84
Aneesh <99677867+aneesh1024@users.noreply.github.com>
2022-09-10 12:57:56 +0530

    Added Admin Dashboard CSS and JS files

---
+++ Admin Dashboard/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productumber: "29123",
+    PaymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera",
+    productumber: "29113",
+    PaymentStatus: "Refunds",
+    shipping: "Declined",
+  },
+  {
+    productName: "Black Bull Tech Drone",
+    productumber: "17462",
+    PaymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Golden Spy Drone",
+    productumber: "491113",
+    PaymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Red Classic Tech Drone",
+    productumber: "98757",
+    PaymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Doxy X23 Mini Drone",
+    productumber: "50563",
+    PaymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productumber: "94739",
+    PaymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
